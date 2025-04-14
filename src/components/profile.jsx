a4364ef46658959ec38c279275a02ebce586c0f7
ali0954 <r017338l@student.staffs.ac.uk>
2025-04-14 19:56:31 +0100

    Fix file casing and imports

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import "../styles/Profile.css"; // Make sure you have this import to load your styles
+import "../styles/profile.css"; // Make sure you have this import to load your styles
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
